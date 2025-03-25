a74f56b3a44873cfe79c0be3fcc896087b218660
Almas Cp <almascp11@gmail.com>
2025-03-25 16:43:42 +0000

    jason updates

--- next.config.js
+++ next.config.js
@@ -2,4 +2,32 @@
 module.exports = {
     // ... rest of the configuration.
     output: "standalone",
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
   };
+
+
+
+
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/portfolio/' : '',
+  basePath: isProd ? '/portfolio' : '',
+  output: 'export',
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
